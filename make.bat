b884ff69c78458f1fdf87b2625b4c093195f7186
DJSchaffner <Jonas.Wilkens@outlook.com>
2020-07-27 19:21:05 +0200

    - Initial commit - Currently downloads last patch from 28th of May, performs backup of AoE2DE directory and replaces downloaded files - TODOs are in inlined in main.py

---
+++ make.bat
@@ -0,0 +1,8 @@
+@echo off
+pyinstaller --onefile --name "aoe2patch_reverter" src/main.py >nul 2>&1
+@RD /S /Q "src/__pycache__"
+@RD /S /Q "build" >nul 2>&1
+if exist "build" rd /s /q "build"
+DEL "aoe2patch_reverter.spec"
+
+echo Build complete
---
+++ src/main.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import shutil
+import utils
+from getpass import getpass
+
+def check_dotnet():
+  return not (shutil.which("dotnet") == None)
+
+# Backup game folder and zip it up in current directory
+def backup(game_path):
+  shutil.make_archive("backup", "zip", utils.resource_path(game_path), ".", )
+
+# Download depot from steam
+def get_depot(depot_id, manifest_id, username, password):
+  depot_downloader = utils.resource_path("res/DepotDownloader/DepotDownloader.dll")
+  os.system(f"dotnet {depot_downloader} -app {app_id} -depot {depot_id} -manifest {manifest_id} -username {username} -password {password} -dir download")
+
+if __name__ == '__main__':
+  # @TODO Make a GUI for this whole thing
+  # @TODO Check if Dot.NET core is installed
+  # @TODO Generate file list to minimize download size
+  # @TODO Grab manifest IDs from steamdb automatically
+  app_id = "813780"
+  depots = ["813781", "813782", "813783", "813784"]
+  manifests = ["620891448408726573", "8112931571790254060", "8481199905487006177", "5123643355926127017"]
+  download_path = utils.resource_path("download")
+
+  if not (check_dotnet()):
+    print("DOTNET Core required but not found!")
+    sys.exit()
+
+  # Get user input
+  game_path = input("Enter Path to AoE2DE: ")
+  username = input("Username: ")
+  password = getpass()
+
+  # Backup game folder
+  backup(game_path)
+
+  # Get all necessary depots
+  for depot, manifest in zip(depots, manifests):
+    get_depot(depot, manifest, username, password)
+
+  # Move downloaded files to game directory and override files
+  shutil.copytree(download_path, game_path)
