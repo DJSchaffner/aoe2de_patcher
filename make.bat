6683618d1300ee0ac5f4fc9dbd44a2fca14fdb67
DJSchaffner <Jonas.Wilkens@outlook.com>
2020-07-27 20:32:51 +0200

    - Added missing make file parameter for the tool to work

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 @echo off
-pyinstaller --onefile --name "aoe2patch_reverter" src/main.py >nul 2>&1
+pyinstaller --onefile --name "aoe2patch_reverter" src/main.py --add-data "res;res" >nul 2>&1
 @RD /S /Q "src/__pycache__"
 @RD /S /Q "build" >nul 2>&1
 if exist "build" rd /s /q "build"
