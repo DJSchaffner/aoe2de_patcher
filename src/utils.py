c85a6a881da8eac25b65360791a1f4d8282ec836
DJSchaffner <Jonas.Wilkens@outlook.com>
2020-07-28 02:08:10 +0200

    - Adjusted helper function for resources to add res path automatically - Adjusted helper function for resources to use libpath objects instead of strings

--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,25 @@
 # Utils
 import sys
 import os
+import pathlib
+
+def base_path():
+  # Get absolute path to resource, works for dev and for PyInstaller
+  if getattr(sys, 'frozen', False):
+    # PyInstaller creates a temp folder and stores path in _MEIPASS
+    return pathlib.Path(pathlib.sys._MEIPASS)
+  else:
+    return pathlib.Path()
 
 def resource_path(relative_path):
   # Get absolute path to resource, works for dev and for PyInstaller
-  try:
+  if getattr(sys, 'frozen', False):
     # PyInstaller creates a temp folder and stores path in _MEIPASS
-    base_path = sys._MEIPASS
-  except Exception:
-    base_path = os.path.abspath(".")
+    base_path = pathlib.Path(pathlib.sys._MEIPASS)
+  else:
+    base_path = pathlib.Path()
 
-  return os.path.join(base_path, relative_path)
+  return base_path / "res" / relative_path
 
 def clear():   
   _ = os.system('cls')
