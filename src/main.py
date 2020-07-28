a10f0d4104d279385ca97e8acf1e7bfcb8d8b48f
DJSchaffner <Jonas.Wilkens@outlook.com>
2020-07-28 17:04:43 +0200

    - Move whole functionality of main.py into a class to be more object oriented and therefore GUI futureproof - Added a webhook file to read data from steamdb.info - List of Patch IDs is generated automatically now - Change list still has to be properly implemented

--- src/main.py
+++ src/main.py
@@ -1,35 +1,105 @@
 import os
 import sys
 import shutil
-import utils
 import traceback
 import pathlib
+import re
+from bs4 import BeautifulSoup
 from getpass import getpass
 
-# Check if dotnet is available
-def check_dotnet():
-  return not (shutil.which("dotnet") == None)
+import utils
+from webhook import Webhook
 
-# Backup game folder and zip it up in current directory
-def backup(game_path, download_path, backup_path):
-  changed_file_list = list(set(os.listdir(game_path.absolute())).intersection(set(os.listdir(download_path.absolute()))))
+class App:
+  app_id = "813780"
 
-  # Copy all files
-  for file in changed_file_list:
-    if pathlib.Path(game_path / file).is_dir():
-      shutil.copytree((game_path / file).absolute(), (backup_path / file).absolute())
-    else:
-      shutil.copy((game_path / file).absolute(), (backup_path / file).absolute())
+  def __init__(self):
+    # Check for installed dotnet
+    if not (utils.check_dotnet()):
+      print("DOTNET Core required but not found!")
+      sys.exit()
 
-# Placeholder function to restore game from backup
-def restore(game_path, download_path, backup_path):
-  changed_file_list = list(set(os.listdir(game_path.absolute())).intersection(set(os.listdir(download_path.absolute()))))
-  print(changed_file_list)
+    self.depots = ["813781", "813782", "813783", "813784"]
+    self.manifests = ["620891448408726573", "8112931571790254060", "8481199905487006177", "5123643355926127017"]
+    self.webhook = Webhook()
 
-# Download depot from steam
-def get_depot(depot_id, manifest_id, username, password):
-  depot_downloader = utils.resource_path("DepotDownloader/DepotDownloader.dll").absolute()
-  os.system(f"dotnet {depot_downloader} -app {app_id} -depot {depot_id} -manifest {manifest_id} -username {username} -password {password} -dir download")
+    self.game_path = pathlib.Path(input("Enter Path to AoE2DE: "))
+    self.download_path = utils.base_path() / "download"
+    self.backup_path = utils.base_path() / "backup"
+
+    # Remove previous download / backup folder if it exists
+    # Create empty folders afterwards
+    if self.download_path.exists():
+      shutil.rmtree(self.download_path.absolute())
+    self.download_path.mkdir()
+    
+    if self.backup_path.exists():
+      shutil.rmtree(self.backup_path.absolute())
+    self.backup_path.mkdir()
+
+    self.username = input("Username: ")
+    self.password = getpass() 
+
+    self.__get_patch_changes(self.__get_patch_list()[1][1])
+
+  def download_patch(self):  
+    # Get all necessary depots
+    for depot, manifest in zip(self.depots, self.manifests):
+      self.__download_depot(depot, manifest)
+
+  def patch(self):
+    # Copy downloaded files into game path
+    shutil.copytree(self.download_path.absolute(), self.game_path.absolute(), dirs_exist_ok = True)
+
+  # Backup game folder and zip it up in current directory
+  def backup(self):
+    changed_file_list = list(set(os.listdir(self.game_path.absolute())).intersection(set(os.listdir(self.download_path.absolute()))))
+
+    # Copy all files
+    for file in changed_file_list:
+      if pathlib.Path(self.game_path / file).is_dir():
+        shutil.copytree((self.game_path / file).absolute(), (self.backup_path / file).absolute())
+      else:
+        shutil.copy((self.game_path / file).absolute(), (self.backup_path / file).absolute())
+
+  # Placeholder function to restore game from backup
+  def restore(self):
+    changed_file_list = list(set(os.listdir(self.game_path.absolute())).intersection(set(os.listdir(self.download_path.absolute()))))
+    print(changed_file_list)
+
+  # Download depot from steam
+  def __download_depot(self, depot_id, manifest_id):
+    depot_downloader = utils.resource_path("DepotDownloader/DepotDownloader.dll").absolute()
+    os.system(f"dotnet {depot_downloader} -app {self.app_id} -depot {depot_id} -manifest {manifest_id} -username {self.username} -password {self.password} -dir download")
+
+  # Get a list of all patches
+  def __get_patch_list(self):
+    result = []
+
+    soup = BeautifulSoup(self.webhook.query_patch_list(self.app_id).content, "html.parser")
+    tbody = soup.find("tbody")
+    
+    for tr in tbody.findAll("tr"):
+      tds = tr.findAll("td")
+      result.append((tds[0].string, tds[4].string))
+    return result
+
+  # Get the changes for a specific patch
+  # Returns a list of Tuples [(Date, ID), ...]
+  def __get_patch_changes(self, patch_id):
+    result = []
+
+    soup = BeautifulSoup(self.webhook.query_patch(patch_id).content, "html.parser")
+
+    div = soup.find("div", {"class" : "depot-history"})
+
+    inner_divs = div.findAll("div")[1:]
+
+    for depot_div in inner_divs:
+      # @TODO Somehow execute the javascript on the page to load the change history
+      print(depot_div.string)
+
+    return result
 
 if __name__ == '__main__':
   # @TODO Make a GUI for this whole thing
@@ -39,45 +109,20 @@ if __name__ == '__main__':
   # @TODO Add restore functionality
   # @TODO Improve backup mechanism
   try:
-    app_id = "813780"
-    depots = ["813781", "813782", "813783", "813784"]
-    manifests = ["620891448408726573", "8112931571790254060", "8481199905487006177", "5123643355926127017"]
-    download_path = utils.base_path() / "download"
-    backup_path = utils.base_path() / "backup"
+    app = App()
+    '''
+    print("Starting download of files")
+    app.download_patch()
+    print("Finished downloading files!")
 
-    # Check for installed dotnet
-    if not (check_dotnet()):
-      print("DOTNET Core required but not found!")
-      sys.exit()
-
-    # Get user input
-    game_path = pathlib.Path(input("Enter Path to AoE2DE: "))
-    username = input("Username: ")
-    password = getpass()
-
-    # Remove previous download / backup folder if it exists
-    # Create empty folders afterwards
-    if download_path.exists():
-      shutil.rmtree(download_path.absolute())
-    download_path.mkdir()
-    
-    if backup_path.exists():
-      shutil.rmtree(backup_path.absolute())
-    backup_path.mkdir()
-
-    # Get all necessary depots
-    for depot, manifest in zip(depots, manifests):
-      get_depot(depot, manifest, username, password)
-
-    # Backup game folder
     print("Starting backup of AoE2DE folder")
-    backup(game_path, download_path, backup_path)
+    app.backup()
     print("Finished backup!")
 
-    # Move downloaded files to game directory and override files
     print("Copying downloaded files")
-    shutil.copytree(download_path.absolute(), game_path.absolute(), dirs_exist_ok = True)
+    app.patch()
     print("Done!")
+    '''
 
     # Wait for user input to close
     input("Press enter to exit...")
