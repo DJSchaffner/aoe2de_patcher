435cddaf59f1934b55c07cb61578007ff48da4a2
DJSchaffner <Jonas.Wilkens@outlook.com>
2020-07-31 15:43:09 +0200

    - Added a GUI to the app - Added a Redirector class - Redirect stdout to GUI for easy switch between GUI and nongui because everything just goes to stdout - Minor tweaks (Renamed some variables / functions, Removed / Added comma here and there, ...) - Only proceed with patching process as long as everything went ok so far - Better integration of the DepotDownloader app regarding input, error handling, etc. - Removed / Added TODOs

--- src/main.py
+++ src/main.py
@@ -7,8 +7,15 @@ from getpass import getpass
 from steam.client import SteamClient
 from enum import IntEnum
 import datetime
+import tkinter as tk
+import tkinter.ttk as ttk
+import tkinter.filedialog
+import tkinter.simpledialog
+import pexpect
+import pexpect.popen_spawn
 
 import utils
+import redirector
 from webhook import Webhook
 
 class Languages(IntEnum):
@@ -22,7 +29,7 @@ class Languages(IntEnum):
   ZH = 7,
   ZHH = 8
 
-class App:
+class App():
   app_id = 813780
   ignored_depots = [
     228987,  # VC 2017 Redist
@@ -43,7 +50,7 @@ class App:
     1022222, # MX
     1022223, # ZH
     1022224, # ZH-Hant
-    1022225, # ES
+    1022225  # ES
   ]
 
   def __init__(self):
@@ -52,87 +59,240 @@ class App:
       print("DOTNET Core required but not found!")
       sys.exit()
 
+    # Set up some variables
     self.webhook = Webhook()
+    self.download_dir = utils.base_path() / "download"
+    self.backup_dir = utils.base_path() / "backup"
     self.patch_list = self.webhook.query_patch_list(self.app_id)
-    self.selected_patch = self.patch_list[0]
-    self.selected_language = Languages.EN
 
-    self.game_path = pathlib.Path(input("Enter Path to AoE2DE: "))
-    self.download_path = utils.base_path() / "download"
-    self.backup_path = utils.base_path() / "backup"
+    # Set up GUI
+    self.window = tk.Tk()
+    self.window.title("AoE2DE Patch Reverter")
+    self.window.minsize(width=700, height=500)
+    self.window.resizable(0, 0)
+    
+    self.upper_frame = tk.Frame(master=self.window)
+    self.upper_frame.pack(side="top", expand=True, fill="both", padx=10, pady=(10, 5))
+    self.upper_frame.columnconfigure(0, weight=1)
+    self.upper_frame.columnconfigure(1, weight=1)
+    self.upper_frame.columnconfigure(2, weight=1)
+    self.upper_frame.columnconfigure(3, weight=1)
+    self.upper_frame.columnconfigure(4, weight=1)
+    self.upper_frame.columnconfigure(5, weight=1)
+    self.upper_frame.rowconfigure(0, weight=1)
+    self.upper_frame.rowconfigure(1, weight=1)
+    self.upper_frame.rowconfigure(2, weight=1)
 
-    self.username = input("Username: ")
-    self.password = getpass()
+    self.lower_frame = tk.Frame(master=self.window)
+    self.lower_frame.pack(side="bottom", expand=True, fill="both", padx=10, pady=(5, 10))
 
-  def download_patch(self):  
-    """Download the patch that has been set for the app."""
+    self.selected_patch_title = tk.StringVar()  
+    self.lbl_select_patch = ttk.Label(master=self.upper_frame, text="Version")
+    self.lbl_select_patch.grid(row=0, column=0, sticky="e")  
+    self.opt_select_patch = ttk.OptionMenu(self.upper_frame, self.selected_patch_title, self.patch_list[0]['title'], *[p['title'] for p in self.patch_list])
+    self.opt_select_patch.grid(row=0, column=1, sticky="w")
+
+    self.selected_language_name = tk.StringVar()
+    self.lbl_select_language = ttk.Label(master=self.upper_frame, text="Language")
+    self.lbl_select_language.grid(row=1, column=0, sticky="e")  
+    self.opt_select_language = ttk.OptionMenu(self.upper_frame, self.selected_language_name, Languages.EN.name, *[l.name for l in Languages])
+    self.opt_select_language.grid(row=1, column=1, sticky="w")
+
+    self.btn_patch = ttk.Button(master=self.upper_frame, text="Patch", command=self.__patch)
+    self.btn_patch.grid(row=0, column=5, sticky="nesw")
+
+    self.btn_restore = ttk.Button(master=self.upper_frame, text="Restore", command=self.__restore)
+    self.btn_restore.grid(row=1, column=5, sticky="nesw")
+
+    self.btn_game_dir = ttk.Button(master=self.upper_frame, text="Set Game directory", command=self.__select_game_dir)
+    self.btn_game_dir.grid(row=2, column=5, sticky="nesw")
+
+    self.lbl_username = ttk.Label(master=self.upper_frame, text="Username")
+    self.lbl_username.grid(row=2, column=0, sticky="e")
+    self.ent_username = ttk.Entry(master=self.upper_frame)
+    self.ent_username.grid(row=2, column=1, sticky="nesw")
+
+    self.lbl_password = ttk.Label(master=self.upper_frame, text="Password")
+    self.lbl_password.grid(row=2, column=2, sticky="e")
+    self.ent_password = ttk.Entry(master=self.upper_frame, show="*")
+    self.ent_password.grid(row=2, column=3, sticky="nesw")
+
+    self.text_box = tk.Text(master=self.lower_frame, state="disabled")
+    self.text_box.pack(expand=True, fill="both")
+
+    # Redirect stdout to the text box
+    sys.stdout = redirector.StdoutRedirector(self.text_box)
+
+  def start(self):
+    """Start the application."""
+
+    self.window.mainloop()
+
+  def __select_game_dir(self):
+    """Open a file dialog for the user to select the game folder."""
+    # @TODO validate its actually the AoE directory (Check for aoe exe)
+    self.game_dir = pathlib.Path(tk.filedialog.askdirectory())
+
+  def __download_patch(self):  
+    """Download the __patch that has been set for the app."""
     
     depots = self.__get_depot_list()
     update_list = []
+
+    # Retrieve selected patch
+    selected_patch = next((p for p in self.patch_list if p['title'] == self.selected_patch_title.get()), None)
+    # Retrieve selected language
+    selected_language = next((l.value for l in Languages if l.name == self.selected_language_name.get()), None)
     
     # Remove previous download folder if it exists
     # Create empty folders afterwards
-    if self.download_path.exists():
-      shutil.rmtree(self.download_path.absolute())
-    self.download_path.mkdir()
+    if self.download_dir.exists():
+      shutil.rmtree(self.download_dir.absolute())
+    self.download_dir.mkdir()
     
     # Loop all depots and insert necessary ones with the latest version to the list of updates
     # @TODO Only add the depots that NEED to be updated depending on which version is currently installed
     for depot in depots:
       # Skip depots that are being ignored
       if  ( (not (depot in self.ignored_depots)) and 
-            ((not (depot in self.language_depots)) or (self.language_depots[self.selected_language] == depot)) ):
+            ((not (depot in self.language_depots)) or (self.language_depots[selected_language] == depot)) ):
 
         manifests = self.webhook.query_manifests(depot)
 
         # Discard empty manifest lists
         if len(manifests) > 0:
-          update_list.append({ 'depot' : depot, 'manifest' : next((m for m in manifests if m['date'] <= self.selected_patch['date']), None) })      
+          update_list.append({ 'depot' : depot, 'manifest' : next((m for m in manifests if m['date'] <= selected_patch['date']), None) })      
 
     # Loop all necessary updates
     for element in update_list:
-      #print(f"depot: {element['depot']} - manifest: {element['manifest']}")
-      self.__download_depot(element['depot'], element['manifest']['id'])
+      if not self.__download_depot(element['depot'], element['manifest']['id']):
+        return False
 
-  def patch(self):
+    return True
+
+  def __move_patch(self):
+    """Move downloaded patch files to game directory"""
+
+    shutil.copytree(self.download_dir.absolute(), self.game_dir.absolute(), dirs_exist_ok = True)
+    return True
+
+  def __patch(self):
     """Start patching the game with the downloaded files."""
 
-    shutil.copytree(self.download_path.absolute(), self.game_path.absolute(), dirs_exist_ok = True)
+    success = True
 
-  def backup(self):
+    # Check some stuff
+    if not hasattr(self, "game_dir"):
+      print("Please select a game directory")
+      return
+
+    if self.ent_username.get() == "":
+      print("Please enter a username")
+      return
+
+    if self.ent_password.get() == "":
+      print("Please enter a password")
+      return
+
+    # Always true
+    if success:
+      print("Downloading patch")
+      success = success and self.__download_patch()
+
+    if success:
+      print("Starting backup")
+      success = success and self.__backup()
+
+    if success:
+      print("Patching files")
+      success = success and self.__move_patch()
+
+    if success:
+      print("DONE!")
+    else:
+      print("Could not patch!")
+
+  def __backup(self):
     """Backup game folder and in current directory."""
 
     # Remove previous backup folder if it exists
     # Create empty folders afterwards
-    if self.backup_path.exists():
-      shutil.rmtree(self.backup_path.absolute())
-    self.backup_path.mkdir()
+    if self.backup_dir.exists():
+      shutil.rmtree(self.backup_dir.absolute())
+    self.backup_dir.mkdir()
 
-    changed_file_list = list(set(os.listdir(self.game_path.absolute())).intersection(set(os.listdir(self.download_path.absolute()))))
+    changed_file_list = list(set(os.listdir(self.game_dir.absolute())).intersection(set(os.listdir(self.download_dir.absolute()))))
 
     # Copy all files
     for file in changed_file_list:
-      utils.copy_file_or_dir(self.game_path, self.backup_path, file)
+      utils.copy_file_or_dir(self.game_dir, self.backup_dir, file)
 
-  def restore(self):
+  def __restore(self):
     """Restores the game directory using the backed up files and downloaded files."""
     
-    backup_file_list = list(os.listdir(self.backup_path.absolute()))
-    download_file_list = list(os.listdir(self.download_path.absolute()))
+    backup_file_list = list(os.listdir(self.backup_dir.absolute()))
+    download_file_list = list(os.listdir(self.download_dir.absolute()))
 
     # Remove added files from the path
+    print("Removing patched files")
     for file in download_file_list:
-      utils.remove_file_or_dir(self.game_path, file)
+      utils.remove_file_or_dir(self.game_dir, file)
+    print("Finished removing patched files")
 
     # Copy backed up files to game path again
+    print("Restoring backup")
     for file in backup_file_list:
-      utils.copy_file_or_dir(self.backup_path, self.game_path, file)
+      utils.copy_file_or_dir(self.backup_dir, self.game_dir, file)
+    print("Finished restoring backup")
+    print("DONE!")
 
   def __download_depot(self, depot_id, manifest_id):
     """Download a specific depot using the manifest id from steam."""
 
-    depot_downloader = utils.resource_path("DepotDownloader/DepotDownloader.dll").absolute()
-    os.system(f"dotnet {depot_downloader} -app {self.app_id} -depot {depot_id} -manifest {manifest_id} -username {self.username} -password {self.password} -dir download")
+    success = False
+    depot_downloader = str(utils.resource_path("DepotDownloader/DepotDownloader.dll").absolute())
+    args = ["dotnet", depot_downloader, "-app", str(self.app_id), "-depot", str(depot_id), "-manifest", str(manifest_id), "-username", str(self.ent_username.get()), "-password", str(self.ent_password.get()), "-dir download"]
+
+    try:
+      p = pexpect.popen_spawn.PopenSpawn(" ".join(args), encoding="utf-8")
+      p.logfile_read = sys.stdout
+
+      responses = [
+        "result: OK",
+        "Error",
+        "Please enter .*: "
+      ]
+
+      # Default timeout in seconds
+      timeout = 15
+      i = p.expect(responses, timeout)
+      sys.stdout.flush()
+
+      # Success
+      if i == 0:
+        success = True
+      # Error
+      elif i == 1:
+        raise ConnectionError("Error logging into account")
+      # Code required
+      elif i == 2:
+        # Open popup for 2FA Code
+        code = tk.simpledialog.askstring(title="Code", prompt="Please enter your login code (2FA / Email)")
+        p.sendline(code)
+
+        if p.expect(responses, timeout=30) == 1:
+          success = False
+        else:
+          raise ConnectionError("Invalid authentication code")
+
+      p.wait()
+      p.expect(pexpect.EOF)
+    except pexpect.exceptions.TIMEOUT as e:
+      print("Error waiting for DepotDownloader to start")
+    except ConnectionError as e:
+      print(e)
+
+    return success
 
   def __get_depot_list(self):
     """Get a list of depots for the app.
@@ -155,47 +315,7 @@ class App:
     return result    
 
 if __name__ == '__main__':
-  # @TODO Make a GUI for this whole thing
+  # @TODO Make GUI look nice
   # @TODO Generate file list to minimize download size
-  # @TODO Grab Update list from steamdb automatically
-  try:
-    app = App()
-    action = 0
-
-    while True:
-      while not (action in [1, 2, 3]):
-        print("1: Patch game")
-        print("2: Restore game")
-        print("3: Exit")
-        action = int(input("Selection: "))
-
-      print()
-
-      if action == 1:
-        print("Starting download of files")
-        #app.download_patch()
-        print("Finished downloading files!")
-
-        print("Starting backup of AoE2DE folder")
-        app.backup()
-        print("Finished backup!")
-
-        print("Copying downloaded files")
-        app.patch()
-        print("Finished copying files!")
-        action = 0
-      elif action == 2:
-        print("Restoring directory")
-        app.restore()
-        print("Finished restoring directory!")
-        action = 0
-      elif action == 3:
-        break
-
-      print()
-
-    # Wait for user input to close
-    input("Press enter to exit...")
-  except BaseException:
-    print(traceback.format_exc())
-    input("")
+  app = App()
+  app.start()
---
+++ src/redirector.py
@@ -0,0 +1,13 @@
+import utils
+
+class IORedirector(object):
+  def __init__(self, text_widget):
+    # Store the widget
+    self.text_widget = text_widget
+
+class StdoutRedirector(IORedirector):
+  def write(self, text):
+    utils.log(self.text_widget, text)
+
+  def flush(self):
+    pass
