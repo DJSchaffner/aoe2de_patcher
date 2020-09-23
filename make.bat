c0581949a5ddf04a6923305832feb8c6970d0a40
DJSchaffner <Jonas.Wilkens@outlook.com>
2020-09-23 20:19:35 +0200

    - Adjusted make file to represent new name

--- make.bat
+++ make.bat
@@ -1,8 +1,8 @@
 @echo off
-pyinstaller --onefile --name "aoe2patch_reverter" src/main.py --add-data "res;res" --noconsole >nul 2>&1
+pyinstaller --onefile --name "aoe2de_patcher" src/main.py --add-data "res;res" --noconsole >nul 2>&1
 @RD /S /Q "src/__pycache__"
 @RD /S /Q "build" >nul 2>&1
 if exist "build" rd /s /q "build"
-DEL "aoe2patch_reverter.spec"
+DEL "aoe2de_patcher.spec"
 
 echo Build complete
