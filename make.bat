37003f259b580d495e6af9707d3b9380b1f573ca
DJSchaffner <Jonas.Wilkens@outlook.com>
2020-11-10 13:51:33 +0100

    - Added noconfirm parameter so pyinstaller doesnt get stuck in the makefile

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 @echo off
-pyinstaller --name "aoe2de_patcher" src/main.py --add-data "res;res" --noconsole >nul 2>&1
+pyinstaller --noconfirm --name "aoe2de_patcher" src/main.py --add-data "res;res" --noconsole >nul 2>&1
 @RD /S /Q "src/__pycache__"
 @RD /S /Q "build" >nul 2>&1
 if exist "build" rd /s /q "build"
