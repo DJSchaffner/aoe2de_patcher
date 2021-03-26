6a64d715eb025b114666fcff6d07d0462c35bc65
DJSchaffner <Jonas.Wilkens@outlook.com>
2021-03-27 00:13:01 +0100

    No longer access steamdb directly within the code so nothing can break when they change stuff This requires more data in patches.json and patching to undocumented patches is no longer possible, as well as patching forward (for now)

--- src/webhook.py
+++ src/webhook.py
@@ -6,14 +6,11 @@ import json
 import requests
 from bs4 import BeautifulSoup
 import steam.webapi
+import cloudscraper
 
 import utils
 
 class Webhook:
-  # For some reason steamdb allows twitterbot to read all sites (https://steamdb.info/robots.txt)
-  # @TODO prevent this from breaking in case steamdb decides to block twitterbot user-agent... avoid detection / rotate user agents / use selenium
-  headers = {'User-Agent' : 'Twitterbot/1.0'}
-
   def query_latest_version(self):
     """Query the latest version number of the tool
     
@@ -24,64 +21,13 @@ class Webhook:
 
     return result
 
-  def query_patch_change_list(self):
-    """Query a list of changed depots for all patches.
+  def query_patches(self):
+    """Query a list of all patches.
     
-    Returns a list of patches and their changed depots"""
+    Returns a list of patches"""
     url = "https://raw.githubusercontent.com/DJSchaffner/AoE2PatchReverter/master/remote/patches.json"
     response = self._query_website(url)
     result = json.loads(response.content)["patches"]
-
-    return result
-
-  def query_manifests(self, depot_id):
-    """Query steamdb.info for a list of manifests for a specific depot and return that list.
-
-    Returns a list of manifests
-    """
-    url = f"https://steamdb.info/depot/{depot_id}/manifests/"
-    response = self._query_website(url, headers=self.headers)
-    result = []
-
-    soup = BeautifulSoup(response.content, "html.parser")
-    div = soup.find("div", {'id' : 'manifests'})
-    tbody = div.find("tbody")
-
-    # Prevent Error for depots without history
-    if not tbody is None:    
-      for tr in tbody.findAll("tr"):
-        tds = tr.findAll("td")
-
-        # Convert the text of the element into a date
-        date = utils.extract_date(tds[0].text)
-        id = tds[2].text
-
-        result.append({ 'date' : date, 'id' : id })
-
-    return result
-
-  def query_patch_list(self, app_id: int, from_date: time.localtime):
-    """Query steamdb.info for a list of patches for a specific app id.
-
-    Returns a list of patches
-    """
-    params = {'appid' : app_id, 'count': 999999}
-    result = []
-    response = steam.webapi.webapi_request("https://api.steampowered.com/ISteamNews/GetNewsForApp/v1", "GET", None, None, params)
-
-    # @TODO Add check for valid response
-
-    for article in response['appnews']['newsitems']['newsitem']:
-      # Search for patches in newsitems (At time of writing patch news have a 5+ digit patch version in the title)
-      version_re = re.search(r"^.* (\d{5,})$", article['title'])
-
-      # Only add actual patches from news feed to list (matches regex)
-      if not version_re is None:        
-        version = int(version_re.group(1))
-        date = time.gmtime(article['date'])
-
-        if date > from_date:
-          result.append({ 'version': version, 'date': date})
 
     return result
 
@@ -91,15 +37,17 @@ class Webhook:
     Returns the content of the found file or None if the file could not be found"""
     url = f"https://raw.githubusercontent.com/DJSchaffner/AoE2PatchReverter/master/remote/{version}/{depot_id}.txt"
     response = self._query_website(url, ignore_success=True)
-    result = None
-
-    if self._is_response_successful(response):
-      result = response.content.decode("utf-8")
+    result = response.content.decode("utf-8")
 
     return result
 
   def _query_website(self, url: str, headers=None, ignore_success=False):
-    response = requests.get(url, headers=headers)
+    # Doesn't work with cloudflare blocking access
+    #response = requests.get(url, headers=headers)
+
+    # Workaround for the cloudflare protection
+    scraper = cloudscraper.create_scraper()
+    response = scraper.get(url, headers=headers)
 
     if (not ignore_success) and (not self._is_response_successful(response)):
       self._print_response_error(response)
