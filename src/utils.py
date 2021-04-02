fbbc39cd52987d1b95de2ea195da017e657e1d85
DJSchaffner <Jonas.Wilkens@outlook.com>
2021-04-02 14:23:06 +0200

    Redid documentation Added previously missing documentation Specified some parameter types that were not yet specified

--- src/utils.py
+++ src/utils.py
@@ -7,35 +7,65 @@ import win32api
 from tkinter import Text
 
 def get_version_number (path: pathlib.Path):
-  """Retrieve the version number of a binary file."""  
+  """Retrieve the version number of a binary file.
+
+  Args:
+      path (pathlib.Path): The path to the file
+
+  Returns:
+      list: Windows version number
+  """
   info = win32api.GetFileVersionInfo(str(path), "\\")
   ms = info['FileVersionMS']
   ls = info['FileVersionLS']
   return win32api.HIWORD (ms), win32api.LOWORD (ms), win32api.HIWORD (ls), win32api.LOWORD (ls)
 
 def log(text_widget: Text, text: str):
-  """Logs a given string to the text widget."""
+  """Logs a given string to the text widget.
+
+  Args:
+      text_widget (Text): The text widget
+      text (str): The text
+  """
   text_widget.configure(state="normal")
   text_widget.insert("end", text)
   text_widget.configure(state="disabled")
   text_widget.see("end")
 
 def copy_file_or_dir(source_dir: pathlib.Path, target_dir: pathlib.Path, file: str):
-  """Copies a file or a directory recursively into the target directory."""
+  """Copies a file or a directory recursively into the target directory.
+
+  Args:
+      source_dir (pathlib.Path): The source directory
+      target_dir (pathlib.Path): The target directory
+      file (str): The file or directory name
+  """
   if (source_dir / file).is_dir():
     shutil.copytree((source_dir / file).absolute(), (target_dir / file).absolute())
   else:
     shutil.copy((source_dir / file).absolute(), (target_dir / file).absolute())
 
-def remove_file_or_dir(dir, file):
-  """Removes a file or directory recursively. Does not throw an error if file does not exist."""  
+def remove_file_or_dir(dir: pathlib.Path, file: str):
+  """Removes a file or directory recursively. Does not throw an error if file does not exist.
+
+  Args:
+      dir (pathlib.Path): The directory
+      file (str): The file or directory name
+  """
   if (dir / file).is_dir():
     shutil.rmtree((dir / file).absolute(), ignore_errors=True)
   else:
     (dir / file).unlink(missing_ok=True)
 
 def backup_files(original_dir: pathlib.Path, override_dir: pathlib.Path, backup_dir: pathlib.Path, debug_info: bool):
-  """Recursively performs backup of original_dir to backup_dir assuming all files/folder from override_dir will be patched."""
+  """Recursively performs backup of original_dir to backup_dir assuming all files/folder from override_dir will be patched.
+
+  Args:
+      original_dir (pathlib.Path): The original directory
+      override_dir (pathlib.Path): The directory containing files / directories that will be overridden
+      backup_dir (pathlib.Path): The directory where the backup will be placed
+      debug_info (bool): Flag for printing debug info
+  """
   changed_file_list = list(set(os.listdir(original_dir.absolute())).intersection(set(os.listdir(override_dir.absolute()))))
 
   for file in changed_file_list:
@@ -51,7 +81,16 @@ def backup_files(original_dir: pathlib.Path, override_dir: pathlib.Path, backup_
       copy_file_or_dir(original_dir, backup_dir, file)
 
 def remove_patched_files(original_dir: pathlib.Path, override_dir: pathlib.Path, debug_info: bool):
-  """Recursively removes all patched files assuming original_dir has been patched with all files from override_dir."""
+  """Recursively removes all patched files assuming original_dir has been patched with all files from override_dir.
+
+  Args:
+      original_dir (pathlib.Path): The original directory
+      override_dir (pathlib.Path): The directory containing the files that have been overridden
+      debug_info (bool): Flag for printing debug info
+
+  Raises:
+      BaseException: If there was an error removing files
+  """
   changed_file_list = os.listdir(override_dir.absolute())
 
   # Remove all overridden files
@@ -77,11 +116,19 @@ def remove_patched_files(original_dir: pathlib.Path, override_dir: pathlib.Path,
     raise e
 
 def check_dotnet():
-  """Checks if dotnet is available."""
+  """Checks if dotnet is available.
+
+  Returns:
+      bool: True if dotnext is available
+  """
   return not (shutil.which("dotnet") is None)
 
 def base_path():
-  """Construct the base path to the exe / project."""
+  """Construct the base path to the exe / project.
+
+  Returns:
+      pathlib.Path: The base path of the exectuable or project
+  """ 
   # Get absolute path to resource, works for dev and for PyInstaller
   if getattr(sys, 'frozen', False):
     # PyInstaller creates a temp folder and stores path in _MEIPASS
@@ -89,8 +136,15 @@ def base_path():
   else:
     return pathlib.Path()
 
-def resource_path(relative_path):
-  """Construct the resource patch for a resource."""
+def resource_path(relative_path: str):
+  """Construct the resource patch for a resource.
+
+  Args:
+      relative_path (str): The path relative to the resource path
+
+  Returns:
+      pathlib.Path: The path to the given resource
+  """
   # Get absolute path to resource, works for dev and for PyInstaller
   if getattr(sys, 'frozen', False):
     # PyInstaller creates a temp folder and stores path in _MEIPASS
@@ -101,5 +155,6 @@ def resource_path(relative_path):
   return base_path / "res" / relative_path
 
 def clear():   
-  """Clear the screen of the console."""  
+  """Clear the screen of the console.
+  """
   _ = os.system('cls')
--- src/webhook.py
+++ src/webhook.py
@@ -10,9 +10,11 @@ import utils
 
 class Webhook:
   def query_latest_version(self):
-    """Query the latest version number of the tool
-    
-    Return the latest version number"""
+    """Returns the latest version of the patch tool.
+
+    Returns:
+        float: The latest version of the patch tool
+    """
     url = "https://raw.githubusercontent.com/DJSchaffner/AoE2PatchReverter/master/remote/version.txt"
     response = self._query_website(url)
     result = float(response.content)
@@ -21,8 +23,10 @@ class Webhook:
 
   def query_patches(self):
     """Query a list of all patches.
-    
-    Returns a list of patches"""
+
+    Returns:
+        list: A list of all documented patches
+    """
     url = "https://raw.githubusercontent.com/DJSchaffner/AoE2PatchReverter/master/remote/patches.json"
     response = self._query_website(url)
     result = json.loads(response.content)["patches"]
@@ -32,14 +36,31 @@ class Webhook:
   def query_filelist(self, version: int, depot_id: int):
     """Query a file list for a certain version and depot it.
 
-    Returns the content of the found file or None if the file could not be found"""
+    Args:
+        version (int): The version to be queried
+        depot_id (int): The depot id to be queried
+
+    Returns:
+        string: The content of the found file or None if the file could not be found
+    """
     url = f"https://raw.githubusercontent.com/DJSchaffner/AoE2PatchReverter/master/remote/{version}/{depot_id}.txt"
     response = self._query_website(url, ignore_success=True)
     result = response.content.decode("utf-8")
 
     return result
 
-  def _query_website(self, url: str, headers=None, ignore_success=False):
+  def _query_website(self, url: str, headers: dict=None, ignore_success: bool=False):
+    """Query a website with the given headers.
+
+    Args:
+        url (str): The url of the website to be queried
+        headers (dict, optional): The headers to use. Defaults to None.
+        ignore_success (bool, optional): If set to true, check if response is 200 and exit program if it is not. Defaults to False.
+
+    Returns:
+        requests.Response: The response of the request
+    """
+
     # Doesn't work with cloudflare blocking access
     response = requests.get(url, headers=headers)
 
@@ -52,10 +73,18 @@ class Webhook:
   def _is_response_successful(self, response: requests.Response):
     """Checks if a response returned successfully.
 
-    Return True/False
+    Args:
+        response (requests.Response): The response to check
+
+    Returns:
+        bool: True if successful (status code 200)
     """
     return response.status_code == 200
 
   def _print_response_error(self, response: requests.Response):
-    """Print the according error for a response."""
+    """Print the according error for a response.
+
+    Args:
+        response (requests.Response): The response containing the error code
+    """
     print(f"Error in HTML request: {response.status_code} ({response.url})")
