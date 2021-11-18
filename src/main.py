6a66827b4c63d822bdd0a011f9eabbe96be8980d
DJSchaffner <Jonas.Wilkens@outlook.com>
2021-11-18 14:37:01 +0100

    Bump version

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ class App():
     self.logic = Logic()
     self.patch_list = list(reversed(self.logic.get_patch_list()))
 
-    self.version = 1.20
+    self.version = 1.30
 
     # Set up GUI
     self.window = tk.Tk()
