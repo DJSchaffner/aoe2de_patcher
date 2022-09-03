9398c49736f65e46ea10f16d5be021beafd193df
DJSchaffner <jonas.wilkens@outlook.com>
2022-09-04 01:32:43 +0200

    changed to virtual environment and nuikta for compiling

--- make.bat
+++ make.bat
@@ -1,8 +1,4 @@
-@echo off
-pyinstaller --noconfirm --name "aoe2de_patcher" src/main.py --add-data "res;res" --noconsole >nul 2>&1
-@RD /S /Q "src/__pycache__"
-@RD /S /Q "build" >nul 2>&1
-if exist "build" rd /s /q "build"
-DEL "aoe2de_patcher.spec"
-
+call .venv\Scripts\activate
+call python -m nuitka --enable-plugin=tk-inter --include-data-dir=res=res --standalone --follow-imports --remove-output src/main.py
+call .venv\Scripts\deactivate
 echo Build complete
