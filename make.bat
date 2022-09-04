8cf5bd2e06ebf1b97ca6426b448cbb76721e251f
DJSchaffner <DJSchaffner@outlook.com>
2022-09-04 23:01:33 +0200

    moved to new python compiler, update makefiles

--- make.bat
+++ make.bat
@@ -1,4 +1,4 @@
 call .venv\Scripts\activate
-call python -m nuitka --enable-plugin=tk-inter --include-data-dir=res=res --standalone --follow-imports --remove-output src/main.py
+call python -m nuitka --enable-plugin=tk-inter --include-data-dir=res=res --standalone --follow-imports --remove-output --windows-disable-console src/main.py
 call .venv\Scripts\deactivate
 echo Build complete
