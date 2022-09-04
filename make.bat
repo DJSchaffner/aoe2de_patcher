063aecde824794f4f3de1b1c9764cb0429322ea8
DJSchaffner <DJSchaffner@outlook.com>
2022-09-04 23:16:38 +0200

    update file namechange in makefiles

--- make.bat
+++ make.bat
@@ -1,4 +1,4 @@
 call .venv\Scripts\activate
-call python -m nuitka --enable-plugin=tk-inter --include-data-dir=res=res --standalone --follow-imports --remove-output --windows-disable-console src/main.py
+call python -m nuitka --enable-plugin=tk-inter --include-data-dir=res=res --standalone --follow-imports --remove-output --windows-disable-console src/aoe2de_patcher.py
 call .venv\Scripts\deactivate
 echo Build complete
