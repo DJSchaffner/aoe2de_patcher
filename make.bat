591700a8b7c112c59f67ef8adfc39f72e17e23bd
DJSchaffner <Jonas.Wilkens@outlook.com>
2024-03-15 20:47:35 +0100

    update makefile to copy dlls

--- make.bat
+++ make.bat
@@ -1,4 +1,5 @@
 call .venv\Scripts\activate
 call python -m nuitka --enable-plugin=tk-inter --include-data-dir=res=res --standalone --follow-imports --remove-output --windows-disable-console src/aoe2de_patcher.py
 call .venv\Scripts\deactivate
+xcopy res aoe2de_patcher.dist\res /s /y
 echo Build complete
