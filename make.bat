83b7399e2113c941127567e87804c5ecd424faac
DJSchaffner <Jonas.Wilkens@outlook.com>
2025-09-17 19:34:12 +0200

    Use better makefile setup

--- make.bat
+++
@@ -1,5 +0,0 @@
-call .venv\Scripts\activate
-call python -m nuitka --enable-plugin=tk-inter --include-data-dir=res=res --standalone --follow-imports --remove-output --windows-disable-console src/aoe2de_patcher.py
-call .venv\Scripts\deactivate
-xcopy res aoe2de_patcher.dist\res /s /y
-echo Build complete
