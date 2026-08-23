8662855aaec3e50baf57d208ed909f9f150d5971
Jonas <Jonas.Wilkens@outlook.com>
2026-08-23 15:55:38 +0200

    Keep track of installed version in UI

--- src/logic.py
+++ src/logic.py
@@ -132,7 +132,6 @@ class Logic:
         self.game_dir = dir
 
         print(f"Game directory set to: {dir.absolute()}")
-        print(f"Installed version detected: {utils.get_game_version(self.game_dir)}")
 
     def get_patch_list(self) -> list[dict]:
         """Returns the patch list.
