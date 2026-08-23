b3bf3275a1b9b002967c3ce9b9cf508c94b99d50
Jonas <Jonas.Wilkens@outlook.com>
2026-08-23 16:20:58 +0200

    Improve version check and drop remote version file requirement

--- src/web_helper.py
+++ src/web_helper.py
@@ -1,7 +1,7 @@
 import json
-from typing import Any
 
 import requests
+from requests import Response
 
 
 class WebHelper:
@@ -11,9 +11,19 @@ class WebHelper:
         Returns:
             major, minor: The latest version of the patch tool (ex: 2, 0)
         """
-        url = "https://raw.githubusercontent.com/DJSchaffner/aoe2de_patcher/master/remote/version.txt"
-        response = self._query_website(url)
-        major, minor = list(map(int, response.text.split(".")))
+        url = "https://api.github.com/repos/DJSchaffner/aoe2de_patcher/releases/latest"
+        response = self._query_website(url, ignore_success=True)
+
+        if not self._is_response_successful(response):
+            print("Could not get latest version")
+            self._print_response_error(response)
+            return 0, 0
+
+        response_json = response.json()
+        tag_name: str = response_json["tag_name"]
+
+        # Version tag format is v<major>.<minor>
+        major, minor = map(int, tag_name.lstrip("v").split("."))
 
         return major, minor
 
@@ -30,7 +40,7 @@ class WebHelper:
 
         return result
 
-    def _query_website(self, url: str, headers: dict | None = None, ignore_success: bool = False) -> Any:
+    def _query_website(self, url: str, headers: dict | None = None, ignore_success: bool = False) -> Response:
         """Query a website with the given headers.
 
         Args:
@@ -39,10 +49,8 @@ class WebHelper:
             ignore_success (bool, optional): If set to true, check if response is 200 and raises an exception if it is not. Defaults to False.
 
         Returns:
-            requests.Response: The response of the request
+            Response: The response of the request
         """
-
-        # Doesn't work with cloudflare blocking access
         response = requests.get(url, headers=headers, timeout=10)
 
         if (not ignore_success) and (not self._is_response_successful(response)):
@@ -51,21 +59,21 @@ class WebHelper:
 
         return response
 
-    def _is_response_successful(self, response: requests.Response) -> bool:
+    def _is_response_successful(self, response: Response) -> bool:
         """Checks if a response returned successfully.
 
         Args:
-            response (requests.Response): The response to check
+            response (Response): The response to check
 
         Returns:
             bool: True if successful (status code 200)
         """
         return response.status_code == 200
 
-    def _print_response_error(self, response: requests.Response) -> None:
+    def _print_response_error(self, response: Response) -> None:
         """Print the according error for a response.
 
         Args:
-            response (requests.Response): The response containing the error code
+            response (Response): The response containing the error code
         """
         print(f"Error in HTML request: {response.status_code} ({response.url})")
